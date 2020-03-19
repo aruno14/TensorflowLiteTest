8faf0fe77c3ceee779fb94a6a2111d7d55610901
arnaud <admin@alpaga.ovh>
2020-03-19 09:34:42 +0900

    Update model and add debug output

--- app/src/main/java/alpaga/ai/tensorflowlitetest/MainActivity.java
+++ app/src/main/java/alpaga/ai/tensorflowlitetest/MainActivity.java
@@ -23,14 +23,18 @@ public class MainActivity extends AppCompatActivity {
             MappedByteBuffer tfliteModelCaption = FileUtil.loadMappedFile(this, "classifier_lstm2.tflite");
             Interpreter.Options options = new Interpreter.Options();
             Interpreter tflite = new Interpreter(tfliteModelCaption, options);
+            String test = "Test: " + tflite.getInputTensorCount()
+                    + " - " + tflite.getInputTensor(0).name() + ":" + Arrays.toString(tflite.getInputTensor(0).shape()) + ":" + tflite.getInputTensor(0).dataType()
+                    + " - " + tflite.getInputTensor(1).name() + ":" + Arrays.toString(tflite.getInputTensor(1).shape()) + ":" + tflite.getInputTensor(1).dataType();
+            Log.d("Tensorflow", test);
             float[][] inputString = new float[1][10];
-            inputString[0][9] = 4;
-            float[][] inputData = new float[1][2];
-            inputData[0][0] = 0;
-            inputData[0][1] = 1;
+            //inputString[0][9] = 4;
+            float[][] inputData = new float[1][10];
+            //inputData[0][0] = 0;
+            //inputData[0][1] = 1;
             Object[] inputArray = {inputString, inputData};
             Map<Integer, Object> outputMap = new HashMap<>();
-            outputMap.put(0, new float[1][61]);
+            outputMap.put(0, new float[1][4]);
             tflite.runForMultipleInputsOutputs(inputArray, outputMap);
             tflite.close();
             float[] result = ((float[][]) outputMap.get(0))[0];
